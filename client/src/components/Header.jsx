c9a4fadc8e66422958316a30645980f2cd0578c7
utkarsh1212-i <utkarshui1212@gmail.com>
2023-03-09 22:54:43 +0530

    Nav bar -Aligned

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import logo from "./assets/logo.png";
 export default function Header() {
   return (
-    <nav className="navbar bg-light mb-4 p-0">
-      <a className="navbar-brand" href="/" />
+    <nav className="navbar text-white bg-dark mb-3 p-2">
       <div className="d-flex">
-        <img src={logo} lat="logo" className="mr-2" />
-        <div>Project MGMT</div>
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="logo" className="mr-2" />
+        </a>
+          <div className="my-2">
+            <h4>Project MGMT</h4>
+          </div>
       </div>
     </nav>
   );
